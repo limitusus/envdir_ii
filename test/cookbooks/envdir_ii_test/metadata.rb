c4e4766fb3a5f0578baeb66a9a834fd5265d10a8
Tomoya Kabe <limit.usus@gmail.com>
2020-07-25 02:43:24 +0900

    Simple test cookbook

--- test/cookbooks/envdir_ii_test/metadata.rb
+++ test/cookbooks/envdir_ii_test/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'limit.usus@gmail.com'
 license 'MIT'
 description 'Installs/Configures envdir_ii_test'
 version '0.1.0'
-chef_version '>= 15.0'
+chef_version '>= 13.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- test/cookbooks/envdir_ii_test/recipes/default.rb
+++ test/cookbooks/envdir_ii_test/recipes/default.rb
@@ -23,3 +23,22 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
+directory "/env" do
+  owner 'root'
+  group 'root'
+end
+
+envdir "/env/default" do
+  owner 'nobody'
+  group 'nogroup'
+  values {
+    "AAA" => {
+      value: "aaa"
+    },
+    "SSS" => {
+      value: "sss",
+      sensitive: true,
+    },
+  }
+end
